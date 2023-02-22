6a02be8c8a501c8ed43498f8092d41b1f75cf78a
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2023-02-22 17:58:32 +0200

    Start task.1 : callBack function filter.

--- allTask.js
+++ allTask.js
@@ -1,1 +1,27 @@
-console.log("!!!!")
+// const mainFilter = (array,callback) => {
+//     const totalArr = []
+//     for (const el of array){
+//         totalArr.push(el)
+//     }
+//     console.log(totalArr)
+// }
+
+const mainFilter = function (array,callback){
+    const totalArr = [];
+    for (const el of array){
+        const passed = callback(el);
+        if(passed) {
+            totalArr.push(el)
+        }
+    }
+    console.log(totalArr)
+}
+const welcomeFunction = (value) => value > 6
+
+mainFilter([1,4,6,7,8,4,66],welcomeFunction)
+//
+// const errorFunction = () => {
+//     console.log("error")
+// }
+
+
