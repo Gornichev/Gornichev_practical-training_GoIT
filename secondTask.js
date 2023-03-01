2cf3701b2b0c9d5ac111de991721ae65fac8a187
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2023-03-01 10:43:38 +0200

    Start task.2 :test tasks ;

--- secondTask.js
+++ secondTask.js
@@ -11,13 +11,25 @@
 // 2) the shortest word search function;
 // Знайти найкоротший єлемент в виразі;
 
-const findShortestWord = string  => {
+// const findShortestWord = string  => {
+//
+//   return string
+//        .split(" ")
+//         .sort((a,b) =>{
+//            return  a.length - b.length
+//         })[0]
+// }
+// console.log(findShortestWord("The a quick brown fox jumped over the lazyyyyyy dog"))
 
-  return string
-       .split(" ")
-        .sort((a,b) =>{
-           return  a.length - b.length
-        })[0]
+const calcFunction = (el,param) => {
+
+const inner = (num) =>{
+    return  el + param * num;
 }
-console.log(findShortestWord("The a quick brown fox jumped over the lazyyyyyy dog"))
+const res = inner(1)
+    console.log(res)
+}
+calcFunction(5,6)
+
+
 
