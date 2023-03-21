e5312e275222e2c6eceacd610384bb5309fe9c35
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2023-03-21 18:25:11 +0200

    innerHTML insertAdjacentHTML event

--- dom.js
+++ dom.js
@@ -1,8 +1,50 @@
-const title = document.querySelector(".userClass")
-const btnUser = document.querySelector(".button-user-click")
+// const title = document.querySelector(".userClass")
+// const btnUser = document.querySelector(".button-user-click")
+//
+// btnUser.addEventListener("click",()=>{
+//     title.classList.toggle("changeColor")
+// })
+//
+// // title.classList.add("changeColor")
 
-btnUser.addEventListener("click",()=>{
-    title.classList.toggle("changeColor")
-})
+// innerHTML==========
+// const btnDecrText = document.querySelector(".js-decrement")
+// btnDecrText.innerHTML = `<button class="js-increment">Збільшити in Btn</button>`
 
-// title.classList.add("changeColor")
+// insertAdjacentHTML==========
+
+// const superTitleRef = document.querySelector(".super-title")
+// superTitleRef.insertAdjacentHTML("beforeend",`__<span style=\"color : #038383 \">element</span>`)
+
+// ===============addEventListener + this
+//use bind.
+
+// const mango = {
+//     username: "Mango",
+//     showUsername() {
+//         console.log(this);
+//         console.log(`My username is: ${this.username}`);
+//     },
+// };
+//
+// const btnUser = document.querySelector(".button-user-click");
+// btnUser.addEventListener("click",mango.showUsername.bind(mango))
+// ===========event
+
+// const btnUser = document.querySelector(".button-user-click");
+// const handleClick = event => {
+//     console.log(event);
+// };
+//
+// btnUser.addEventListener("click", handleClick);
+
+// document.addEventListener("keydown", event => {
+//     console.log("Keydown: ", event);
+// });
+
+const testListRef = document.querySelector(".test-list");
+const firstElement = testListRef.querySelectorAll("li")[1];
+console.log(firstElement)
+
+
+
