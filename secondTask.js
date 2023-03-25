7361e39c145b164d87cd664c496f9c5be6334339
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2023-03-25 16:40:56 +0200

    event target on the form

--- secondTask.js
+++ secondTask.js
@@ -477,40 +477,53 @@
 
 // Task 21
 
-const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
-
-const isLoginValid = function (login) {
-
-    if (login.length >= 4 && login.length <= 16){
-        return true
-    } else {
-        return false
-    }
-
-};
-
-const isLoginUnique = function (allLogins, login) {
-
-    if(allLogins.includes(login)){
-        return false
-    } else {
-        return true
-    }
-
-};
-
-const addLogin = function (allLogins, login) {
-    // твій код
-};
-
-/*
- * Виклич функції для перевірки працездатності твоєї реалізації.
- */
+// const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+//
+// const isLoginValid = function (login) {
+//
+//
+//     if (login.length >= 4 && login.length <= 16){
+//         return true
+//     } else {
+//         return false
+//     }
+//
+// };
+//
+//
+// const isLoginUnique = function (allLogins, login) {
+//
+//
+//     if(allLogins.includes(login)){
+//         return false
+//     } else {
+//         return true
+//     }
+//
+// };
+// console.log(isLoginValid('robotGoogles'))
+//
+// const addLogin = function (allLogins, login) {
+//
+//     if(!isLoginValid(login)){
+//         return 'Помилка! Логін повинен бути від 4 до 16 символів';
+//
+//     }
+//
+//     if(isLoginUnique(login)){
+//         logins.push(login);
+//         return 'Логін успішно доданий!';
+//
+//     }
+//     return 'Такий логін уже використовується!';
+// };
+//
+//
+//
 // console.log(addLogin(logins, 'Ajax')); // 'Логін успішно доданий!'
-// console.log(addLogin(logins, 'robotGoogles')); // 'Такий логін вже використовується!'
+// console.log(addLogin(logins, 'robotGoogles')); // 'Такий логін вже використовується!'   Не правильно щось !
 // console.log(addLogin(logins, 'Zod')); // 'Помилка! Логін повинен бути від 4 до 16 символів'
 // console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Помилка! Логін повинен бути від 4 до 16 символів'
-
 
 
 
