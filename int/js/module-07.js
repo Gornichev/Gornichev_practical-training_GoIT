38b1cfeb1c09e9604f261551542e6849492640ba
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2023-04-06 19:31:41 +0300

    Lodash.Throttle і Debounce

--- int/js/module-07.js
+++ int/js/module-07.js
@@ -1,102 +1,105 @@
-// (event bubbling
-
-const parent = document.querySelector("#parent");
-const child = document.querySelector("#child");
-const descendant = document.querySelector("#descendant");
-
-// parent.addEventListener("click", () => {
-//     alert("Parent click handler");
+// // (event bubbling
+//
+// const parent = document.querySelector("#parent");
+// const child = document.querySelector("#child");
+// const descendant = document.querySelector("#descendant");
+//
+// // parent.addEventListener("click", () => {
+// //     alert("Parent click handler");
+// // });
+// //
+// // child.addEventListener("click", () => {
+// //     alert("Child click handler");
+// // });
+// //
+// // descendant.addEventListener("click", () => {
+// //     alert("Descendant click handler");
+// // });
+// //
+//
+//
+// // parent.addEventListener("click", (event) => {
+// //     console.log("event.target: ", event.target);
+// //     console.log("event.currentTarget: ", event.currentTarget);
+// // });
+//
+// //
+// // parent.addEventListener("click", () => {
+// //     alert(
+// //         "Parent click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
+// //     );
+// // });
+// //
+// // child.addEventListener("click", () => {
+// //     alert(
+// //         "Child click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
+// //     );
+// // });
+// //
+// // descendant.addEventListener("click", (event) => {
+// //     event.stopPropagation();
+// //     alert("Descendant click handler");
+// // });
+// //
+// //
+// const listenerBnt = document.querySelector(`.listenerBtn`);
+//
+//
+// // listenerBnt.addEventListener(`click`,(event)=> {
+// //
+// //     // if (event.target.nodeName !== "BUTTON") {
+// //     //    return;
+// //     // };
+// //     // console.log("use Bubbling");
+// //     console.log(event.target.dataset)
+// //
+// // });
+//
+// const output = document.querySelector(".output");
+// let scrollEventCounter = 0;
+//
+// document.addEventListener("scroll", () => {
+//     scrollEventCounter += 1;
+//     output.textContent = scrollEventCounter;
 // });
 //
-// child.addEventListener("click", () => {
-//     alert("Child click handler");
-// });
+// // document.addEventListener("scroll", _.throttle(() => {
+// //         console.log("Scroll handler call every 300ms");
+// //     }, 300)
+// // );
 //
-// descendant.addEventListener("click", () => {
-//     alert("Descendant click handler");
-// });
+// // document.addEventListener("scroll", _.debounce(() => {
+// //         console.log("Scroll handler call after 300ms pause");
+// //     }, 300)
+// // );
 //
-
-
-// parent.addEventListener("click", (event) => {
-//     console.log("event.target: ", event.target);
-//     console.log("event.currentTarget: ", event.currentTarget);
-// });
-
+// // event-delegation
 //
-// parent.addEventListener("click", () => {
-//     alert(
-//         "Parent click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
-//     );
-// });
+// const tags = document.querySelector(".js-tags");
+// const activeTeg = document.querySelector(".js-active-tag");
 //
-// child.addEventListener("click", () => {
-//     alert(
-//         "Child click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
-//     );
-// });
+// tags.addEventListener("click", onTagClick);
 //
-// descendant.addEventListener("click", (event) => {
-//     event.stopPropagation();
-//     alert("Descendant click handler");
-// });
+// function onTagClick (event){
 //
+//     if(event.target.nodeName !== "BUTTON"){
+//         return;
+//     };
+//     console.log("Btn");
 //
-const listenerBnt = document.querySelector(`.listenerBtn`);
+//     const prevTags = event.currentTarget.querySelector(".tags-bnt-active");
+//
+//     if(prevTags) {
+//         prevTags.classList.remove("tags-bnt-active");
+//     };
+//
+//     const nextTags = event.target;
+//     nextTags.classList.add("tags-bnt-active");
+//
+//     activeTeg.textContent = nextTags.dataset.value
+//
+// };
 
+// Lodash
 
-listenerBnt.addEventListener(`click`,(event)=> {
-
-    // if (event.target.nodeName !== "BUTTON") {
-    //    return;
-    // };
-    // console.log("use Bubbling");
-    console.log(event.target.dataset)
-
-});
-
-const output = document.querySelector(".output");
-let scrollEventCounter = 0;
-
-document.addEventListener("scroll", () => {
-    scrollEventCounter += 1;
-    output.textContent = scrollEventCounter;
-});
-
-// document.addEventListener("scroll", _.throttle(() => {
-//         console.log("Scroll handler call every 300ms");
-//     }, 300)
-// );
-
-// document.addEventListener("scroll", _.debounce(() => {
-//         console.log("Scroll handler call after 300ms pause");
-//     }, 300)
-// );
-
-// event-delegation
-
-const tags = document.querySelector(".js-tags");
-const activeTeg = document.querySelector(".js-active-tag");
-
-tags.addEventListener("click", onTagClick);
-
-function onTagClick (event){
-
-    if(event.target.nodeName !== "BUTTON"){
-        return;
-    };
-    console.log("Btn");
-
-    const prevTags = event.currentTarget.querySelector(".tags-bnt-active");
-    if(prevTags) {
-        prevTags.classList.remove("tags-bnt-active");
-    };
-
-    const nextTags = event.target;
-    nextTags.classList.add("tags-bnt-active");
-
-    activeTeg.textContent = nextTags.dataset.value
-
-};
-
-
+console.log(_.add(2,2))
