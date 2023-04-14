16f443488db6b5bc8ab59b3650e4b96133ccc189
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2023-04-14 21:29:59 +0300

    babel

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,1 +1,21 @@
-console.log("hello nigga")
+import colors from  "../js/main"
+
+colors.map((color) =>{
+    console.log(`${color.hex} and ${color.rgb}`);
+});
+
+console.log("hello nigga");
+
+
+class User {
+    #age
+    constructor(status) {
+        this.status = status;
+        this.#age = age;
+    }
+    getInfo() {
+        console.log(this.#age)
+    }
+};
+
+const viva = new User("incognito", "26")
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,3 +1,11 @@
-module.exports = {
+const path = require("path")
 
+module.exports = {
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'webpack.bundle.js',
+    },
+    module: {
+        rules: [{ test: /\.m?js$/, exclude : /node_modules/,  use: 'babel-loader' }],
+    },
 };
