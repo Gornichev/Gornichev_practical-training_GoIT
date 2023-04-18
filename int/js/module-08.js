8b3ea2bdf45693b656205696d4e2a3dbc7750015
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2023-04-18 17:52:43 +0300

    Json , LocaleStorage

--- int/js/module-08.js
+++ int/js/module-08.js
@@ -1,13 +1,37 @@
 // import  products from  "./module-07.js"
-//
 // console.log(products);
-// console.log("!")
 
 // import numbers from "./module-07.js"
 // console.log(numbers)
 
 // import  person from "./module-07.js";
-//
 // const {name,age} = person;
-//
 // console.log(name)
+
+// localStorage
+// JSON
+// JSON.parse() // read;
+// JSON.stringify() // toString;
+// для того щоб в LocalStorege  записати обьект його спочатку треба привести до стрінг методом JSON.stringify(user).
+
+const user = {
+    age : 25,
+    name : "Yurii",
+    status : true
+};
+
+localStorage.setItem("user-info",JSON.stringify(user));
+
+/// прочитати по ключу
+// localStorage.getItem("user-info")
+const data = localStorage.getItem("user-info");
+
+if (data) {
+    const findUser = JSON.parse(data)
+
+    console.log(findUser)
+};
+
+// Revove
+
+localStorage.removeItem("user-info");
