a258a1472127411a8590ac9a77a7ee3c80bff557
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2023-04-19 22:03:30 +0300

    localStorage

--- int/js/module-08.js
+++ int/js/module-08.js
@@ -9,29 +9,62 @@
 // console.log(name)
 
 // localStorage
+
+// localStorage.setItem();
+// localStorage.getItem();
+// localStorage.removeItem();
+// localStorage.clear();
 // JSON
 // JSON.parse() // read;
 // JSON.stringify() // toString;
 // для того щоб в LocalStorege  записати обьект його спочатку треба привести до стрінг методом JSON.stringify(user).
 
-const user = {
-    age : 25,
-    name : "Yurii",
-    status : true
-};
+// const user = {
+//     age : 25,
+//     name : "Yurii",
+//     status : true
+// };
+//
+// localStorage.setItem("user-info",JSON.stringify(user));
+//
+// /// прочитати по ключу
+// // localStorage.getItem("user-info")
+// const data = localStorage.getItem("user-info");
+//
+// if (data) {
+//     const findUser = JSON.parse(data)
+//
+//     console.log(findUser)
+// };
+//
+// // Revove
+//
+// localStorage.removeItem("user-info");
 
-localStorage.setItem("user-info",JSON.stringify(user));
+// Example Form
 
-/// прочитати по ключу
-// localStorage.getItem("user-info")
-const data = localStorage.getItem("user-info");
+const formRef = document.querySelector("#js-form");
+const textAreaRef = document.querySelector("#js-textarea");
 
-if (data) {
-    const findUser = JSON.parse(data)
+window.addEventListener("load", ()=> {
+    const msg = localStorage.getItem("user-message");
+    if(msg){
+        textAreaRef.textContent = msg;
+    }
+});
 
-    console.log(findUser)
-};
+formRef.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const msg = localStorage.getItem("user-message");
+    if(msg){
+        localStorage.removeItem("user-message")
+        textAreaRef.textContent = "";
+        e.target.reset
+    };
+});
 
-// Revove
+textAreaRef.addEventListener("input", (e)=> {
+    const msg = e.target.value;
+    localStorage.setItem("user-message",msg);
 
-localStorage.removeItem("user-info");
+});
