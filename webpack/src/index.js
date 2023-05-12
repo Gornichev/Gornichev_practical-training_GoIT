ef94907c43c67412975c8ba42a48e68fc80c3d65
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2023-05-12 15:26:35 +0300

    import Bootstrap Native

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -4,15 +4,32 @@ import menuTemplates from "./templates/menu.handlebars";
 import menuData from "./menuData";
 import refs from "../js/refs";
 import lodash from 'lodash.throttle'
-import {Modal} from "bootstrap.native"
 
-const markup = menuTemplates(menuData);
-refs.root.innerHTML = markup;
+import {Modal,Button,Carousel, Collapse, Dropdown} from "bootstrap.native"
 
 const modal = new Modal("#exampleModal");
-modal.show()
+const PROMPT_DELAY = 3000;
+
+function showModal () {
+    modal.show();
+    console.log("open modal window");
+};
+
+setTimeout(showModal,PROMPT_DELAY);
 
 
+const  modalUser = document.querySelector("#exampleModal")
+
+modalUser.addEventListener("hide.bs.modal", ()=> {
+    console.log('!!!')
+})
+
+
+
+
+// colorsPictures
+// const markup = menuTemplates(menuData);
+// refs.root.innerHTML = markup;
 // colors.map((color) =>{
 //     console.log(`${color.hex} and ${color.rgb}`);
 // });
