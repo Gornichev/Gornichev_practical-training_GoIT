5075ccb51a3e85d9fc07a5b700f11b203570b757
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2023-05-31 10:52:01 +0300

    add pageCounter and load only 4(?+-) items

--- int/js/module-10.js
+++ int/js/module-10.js
@@ -6,11 +6,19 @@ const refs = {
     bntReadPersonage : document.querySelector("#js-bnt"),
 };
 
+let pageCounter = 1;
+
 function getData() {
-    fetch("https://rickandmortyapi.com/api/character")
+
+    fetch(`https://rickandmortyapi.com/api/character?page=${pageCounter}`)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
+
+            console.log(pageCounter)
+            // console.log(data.results);
+            // if(data.results.length >= 2) {
+            //     data.results = data.results.slice(0,4); /// завантажить тільки перші 4 елемети масиву
+            // }
             const markup = data.results
                 .map(
                     ({ image, name, status, gender, location, id }) =>
@@ -26,8 +34,9 @@ function getData() {
                 .join("");
 
             refs.itemList.insertAdjacentHTML("beforeend", markup);
+            pageCounter += 1
         });
-}
+};
 
 refs.bntReadPersonage.addEventListener("click",getData);
 
